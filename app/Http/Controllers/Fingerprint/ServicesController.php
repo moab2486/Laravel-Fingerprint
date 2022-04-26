8c34d20fccc60e97baa75bd210435c17e584da2a
Abdulkadir <abdulrasakabdulkadir@yahoo.com>
2022-04-26 09:24:35 +0100

    Fix to variable name in Fingerprint services controller

--- app/Http/Controllers/Fingerprint/ServicesController.php
+++ app/Http/Controllers/Fingerprint/ServicesController.php
@@ -11,12 +11,12 @@ class ServicesController extends Controller
     public function enroll(Request $request){
         $data = $request->all();
 
-        $user = Biometric::where('user_id', $data['user_id'])->get();
+        $user = Biometric::where('user_id', $data['id'])->get();
 
         if($user < 1){
-            $is_fraud = $this->is_duplicate($data->curr_finger_data);
+            $is_fraud = $this->is_duplicate($data->index_finger);
             if($is_fraud == false){
-                $curr_finger_data = $data->curr_finger_data;
+                $curr_finger_data = $data->index_finger;
             
                 $enrolled_finger = enroll_fingerprint($curr_finger_data);
             
@@ -24,8 +24,8 @@ class ServicesController extends Controller
                     # todo: return the enrolled fmds instead
 
                     Biometric::create([
-                        'user_id' => $data['user_id'],
-                        'user_id' => $data['curr_finger_data']
+                        'user_id' => $data['id'],
+                        'finger_print' => $data['index_finger']
                     ]);
                     
                     return response()->json("successful");
@@ -34,7 +34,7 @@ class ServicesController extends Controller
                     return response()->json("enrollment failed!");
                 }
             }else{
-                $verify = $this->verify($data['user_id'], $data['curr_finger_data']);
+                $verify = $this->verify($data['id'], $data['index_finger']);
                 if($verify == "match"){
                     return response()->json("User has previously enrolled");
                 }else{
